53e22ac57385efd36dede88f7643646e5038d873
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2021-12-20 08:40:32 +0200

    changes

--- Carca/Carca.Android/Resources/Resource.designer.cs
+++ Carca/Carca.Android/Resources/Resource.designer.cs
@@ -11824,16 +11824,19 @@ namespace Carca.Droid
 			public const int test_custom_background = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_dark = 2131165340;
+			public const int text = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tooltip_frame_light = 2131165341;
+			public const int tooltip_frame_dark = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int weather = 2131165342;
+			public const int tooltip_frame_light = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int wind = 2131165343;
+			public const int weather = 2131165343;
+			
+			// aapt resource value: 0x7F0700A0
+			public const int wind = 2131165344;
 			
 			static Drawable()
 			{
