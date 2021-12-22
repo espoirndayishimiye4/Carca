9afc1414bb813809e61b9b48ff11649c4108f762
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2021-12-22 15:55:16 +0200

    rounded entry

---
+++ Carca/Carca.Android/MyEntryRenderer.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Carca;
+using Carca.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(MyEntry),typeof(MyEntryRenderer))]
+namespace Carca.Droid
+{
+    class MyEntryRenderer:EntryRenderer
+    { 
+        public MyEntryRenderer(Context context):base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                
+
+                var gradientDrawable = new GradientDrawable();
+
+                gradientDrawable.SetCornerRadius(60f);
+                gradientDrawable.SetStroke(3, Android.Graphics.Color.LightGray);
+
+                Control.SetBackground(gradientDrawable);
+            }
+        }
+    }
+}
---
+++ Carca/Carca.iOS/MyEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Carca;
+using Carca.iOS;
+using CoreGraphics;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace Carca.iOS
+{
+    class MyEntryRenderer:EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(Control != null)
+            {
+                 Control.Layer.CornerRadius = 20;
+                Control.Layer.BorderWidth = 3f;
+                Control.Layer.BorderColor = Color.LightGray.ToCGColor();
+
+                Control.LeftView = new UIKit.UIView(new CGRect(0, 0, 10, 0));
+                Control.LeftViewMode = UIKit.UITextFieldViewMode.Always;
+            }
+        }
+    }
+}
