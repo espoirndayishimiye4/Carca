d263bbb43700f2a2b89a0896b99ba33625abc676
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2021-12-24 12:57:25 +0200

    updates

--- Carca/Carca/Settings.xaml.cs
+++ Carca/Carca/Settings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,22 @@ namespace Carca
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Settings : ContentPage
     {
+        public ObservableCollection<Card> ListDetails{ get; set; }
         public Settings()
         {
             InitializeComponent();
+            ListDetails = new ObservableCollection<Card>
+            {
+                new Card{ ImgIcon = "home.png", Name = "Payment Methods"}
+
+            };
+            BindingContext = this;
         }
+    }
+    public class Card
+    {
+        public string ImgIcon { get; set; }
+
+        public string Name { get; set; }
     }
 }
