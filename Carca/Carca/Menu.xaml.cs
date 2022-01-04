7c966796aa26ee59d8a8125d7261a6177185c10e
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2022-01-04 08:28:57 -0800

    changes

--- Carca/Carca/Menu.xaml.cs
+++ Carca/Carca/Menu.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,36 @@ namespace Carca
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Menu : ContentPage
     {
+        public ObservableCollection<Liste> ListDetails { get; set; }
         public Menu()
         {
             InitializeComponent();
-        }
+
+       
+            List<String> card = new List<String>();
+
+            for (int i = 0; i < 10; i++) { 
+            card.Add("Card");
+            }
+            BindingContext = card;
+            ListDetails = new ObservableCollection<Liste>
+            {
+                    
+
+                new Liste{ Image = "home.png", Name = "Payment Methods"},
+                new Liste{ Image = "home.png", Name = "Payment Methods"},
+                new Liste{ Image = "home.png", Name = "Payment Methods"},
+                new Liste{ Image = "home.png", Name = "Payment Methods"},
+
+            };
+                BindingContext = this;
+            }
+        
+    }
+    public class Liste
+    {
+        public string Image { get; set; }
+
+        public string Name { get; set; }
     }
 }
